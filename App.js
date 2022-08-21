08c5f10e2fe50d93d368581a5032cd4578d6bae8
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2022-08-21 12:02:14 -0300

    importado modulos e criado estrutura de navegação

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import Index from './src/Index/Index.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Index' component={Index}/>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
